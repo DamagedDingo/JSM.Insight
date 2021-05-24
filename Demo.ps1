20a73d36e30e031b001fb2502b8b66d000caf6a5
DamagedDingo <gary.ryan.smith@gmail.com>
2021-05-24 20:16:45 +1000

    Update Demo.ps1

--- Demo.ps1
+++ Demo.ps1
@@ -1,8 +1,11 @@
 # Set Script Defaults
-# These will be passed in automaticly to all other commands in script scope
+# These will be passed in automatically to all other functions in script scope
 Set-InsightServer -Server 'YOUR URL HERE'
 Set-InsightCreds -Username 'YOUR CASE SENSITIVE JIRA USERNAME' -Password 'YOUR JIRA PASSWORD'
 Set-WorkspaceID
+
+# Create a new Object Schema
+New-ObjectSchema -Name "API Test" -objectSchemaKey "API" -Description "A Simple Demo Schema"
 
 # Get Global Icons
 Get-Icons
@@ -13,4 +16,3 @@ Get-Icons -IconID 1
 # Get an object by its Key
 Get-Object -ID "OBJECT ID HERE" # Only Name and ID
 Get-Object -ID "OBJECT ID HERE" -Full # Complete information
-
